b9b87ba5c1824c937fa6449e0d62cd6b6e56ad05
BrianBontempi <brian.bont54@gmail.com>
2024-02-01 16:58:30 +0100

    add emails list and endpoint src

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-const app = vue.createApp({
+console.log('Axios OK', axios);
+
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+const app = Vue.createApp({
     data() {
         return {
-
+            emails: []
         }
     }
 });
 
 app.mount('#root')
+
